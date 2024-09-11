54c52045855580d2a44e1cc38aae6cb65ba79f2b
slimaniabdelhadi <37911339+slimaniabdelhadi@users.noreply.github.com>
2024-09-11 21:48:53 +0100

    Update jvscrpt.js

--- atr/jvscrpt.js
+++ atr/jvscrpt.js
@@ -383,6 +383,47 @@ function cntn() {
     },500)  */
 }
 
+/* instalation app */
+let deferredPrompt;
+/* if app is instal */
+/* window.addEventListener('appinstalled', () => {
+    // Hide the app-provided install promotion
+    gebi('dwnld').className ='n';
+    // Clear the deferredPrompt so it can be garbage collected
+    deferredPrompt = null; */
+/* // Optionally, send analytics event to indicate successful install
+console.log('PWA was installed'); */
+/*  }); */
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    let os = navigator.userAgent.toLocaleLowerCase();
+
+    if (os.includes('android') || os.includes('ipad') || os.includes('iphone')) {
+        gebi('dvdw').className = 'blck';
+    }
+
+});
+
+let buttonInstall = gebi('dwnld');
+buttonInstall.addEventListener('click', async () => {
+    gebi('dvdw').className = 'n';
+    deferredPrompt.prompt();
+    /* if (vUp.dwAapp >0) {
+        
+    } */
+    /* 
+        const { outcome } = await deferredPrompt.userChoice;
+    
+        console.log(`User response to the install prompt: ${outcome}`); */
+    deferredPrompt = null;
+});
+
+
+
+
 function lstWrd() {
     let n = gebi("cntWrds"),
         e = "",
